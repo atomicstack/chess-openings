e7552e01bbcf26e2696c477bed05b17b9f435a5d
Matt Koscica <matt.koscica@gmail.com>
2026-04-18 04:54:05 +0200

    feat: wire ModelContainer with new schema

--- Chess Openings/Chess_OpeningsApp.swift
+++ Chess Openings/Chess_OpeningsApp.swift
@@ -1,10 +1,27 @@
 import SwiftUI
+import SwiftData
 
 @main
 struct Chess_OpeningsApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Opening.self,
+            Line.self,
+            LineProgress.self,
+            UserSettings.self,
+        ])
+        let config = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+        do {
+            return try ModelContainer(for: schema, configurations: [config])
+        } catch {
+            fatalError("could not create model container: \(error)")
+        }
+    }()
+
     var body: some Scene {
         WindowGroup {
             RootView()
         }
+        .modelContainer(sharedModelContainer)
     }
 }
